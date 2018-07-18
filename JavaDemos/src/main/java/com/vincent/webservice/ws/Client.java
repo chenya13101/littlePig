a318195dbdb4fb6184b188ba4529e3a92d18057f
wensen <846384270@qq.com>
2018-07-18 17:46:50 +0800

    classpath修改

--- JavaDemos/src/main/java/com/vincent/webservice/ws/Client.java
+++ JavaDemos/src/main/java/com/vincent/webservice/ws/Client.java
@@ -4,6 +4,7 @@ public class Client {
 	public static void main(String[] args) {
 		BusinessService service = new BusinessService();
 		Business business = service.getBusinessPort();
+		System.out.println("两个业务类都是调用 wsimport生成的客户端辅助类");
 		String response = business.echo("show me the money!!");
 		System.out.println(response);
 	}
