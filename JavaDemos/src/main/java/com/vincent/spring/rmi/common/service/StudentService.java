63135f591843bd7b5b22be4197613adfb0fee859
wensen <846384270@qq.com>
2018-07-19 10:07:40 +0800

    去除多余Import

--- JavaDemos/src/main/java/com/vincent/spring/rmi/common/service/StudentService.java
+++ JavaDemos/src/main/java/com/vincent/spring/rmi/common/service/StudentService.java
@@ -1,7 +1,5 @@
 package com.vincent.spring.rmi.common.service;
 
-import org.springframework.stereotype.Service;
-
 import com.vincent.spring.rmi.common.bean.Student;
 
 public interface StudentService {
