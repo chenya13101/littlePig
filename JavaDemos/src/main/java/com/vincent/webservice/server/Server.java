b7c01702eab16b357f0a4c635594f77e73e528b2
wensen <846384270@qq.com>
2018-07-19 09:58:38 +0800

    修改注释

--- JavaDemos/src/main/java/com/vincent/webservice/server/Server.java
+++ JavaDemos/src/main/java/com/vincent/webservice/server/Server.java
@@ -10,7 +10,6 @@ public class Server {
 		System.out.println("在浏览器中可以看到wsdl,输入: " + wsdlUrl + "?wsdl");
 
 		System.out.println("wsimport生成辅助代码的命令为:" + "wsimport -keep http://localhost:9527/BusinessService?wsdl");
-		// TODO rmi的缺點是什麽缺点
-		System.out.println("rmi的缺點是什麽缺点");
+		System.out.println("rmi的缺點：无法控制通信过程，比如超时，错误，是否需要重发,发送速度等");
 	}
 }
