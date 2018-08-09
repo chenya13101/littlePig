584ecc1929d5fce205731c3b18e346f2f02ea7db
wensen <846384270@qq.com>
2018-08-09 11:59:42 +0800

    按位与原理：新生成的数值 <= 两者中的最大值

---
+++ JavaDemos/src/main/java/com/vincent/math/AndDemo.java
@@ -0,0 +1,18 @@
+package com.vincent.math;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AndDemo {
+
+	@Test
+	public void testAnd() {
+		System.out.println("按位与原理：新生成的数值 <= 两者中的最大值");
+		int i = 5; // 0101
+		int j = 12; // 1100
+		int d = i & j;
+		assertEquals(d, 4);
+	}
+
+}
